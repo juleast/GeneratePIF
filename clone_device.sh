696ce239a50564713b6d46870b740033584d97a6
Juleast <josephhs0815@gmail.com>
2023-12-02 17:29:14 -0500

    Optimize scripts - Change conditionals for renaming from directories to specific file checks - Make core parts of the script functions for cleaner code.
    - Move main code for gen_pif_custom.sh to a function

--- clone_device.sh
+++ clone_device.sh
@@ -1,8 +1,71 @@
-echo "### Git clone script for phone FP dumps ###"
-echo "Script by Juleast"
-echo "*Note you need to specify the git repo URL below:"
+#!/system/bin/sh
+#
+# A repo URL has to be inputed in the input line.
+# Get the repo URL from the Android dumps repo (https://dumps.tadiphone.dev/dumps/)
+# Then go to brand_name --> device_name --> clone button.
 
-read -p "Paste URL here: " url
+# single file checkouts for inputed repo
+checkout() {
+  echo "### Try to get build.prop from system dirs ###"
+  echo
+  git checkout $br -- system/build.prop
+  git checkout $br -- system/system/build.prop
+  echo
+  echo "### Try to get build.prop from system/product dirs ###"
+  echo
+  git checkout $br -- system/system/product/build.prop
+  git checkout $br -- system/product/build.prop
+  echo
+  echo "### Try to get build.prop from vendor dirs ###"
+  echo
+  git checkout $br -- vendor/build.prop 
+  git checkout $br -- system/vendor/build.prop
+
+  rename
+}
+
+# rename and move files that are clones
+rename() {
+  echo Rename and move files...
+  if [ -f "system/product/build.prop" ]; then
+    mv system/product/build.prop ./product-build.prop
+  elif [ -f "system/system/product/build.prop" ]; then
+    mv system/system/product/build.prop ./product-build.prop
+  fi
+
+  if [ -f "system/build.prop" ]; then
+    mv system/build.prop ./
+  elif [ -f "system/system/build.prop" ]; then
+    mv system/system/build.prop ./
+  fi
+
+  if [ -f "system/vendor/build.prop" ]; then
+      mv system/vendor/build.prop ./vendor-build.prop
+  fi
+  if [ -f "vendor/build.prop" ]; then
+    mv vendor/build.prop ./vendor-build.prop
+  fi
+
+  delete
+}
+
+# delete empty folders when finished
+delete() {
+  if [ -d "system" ]; then
+  rm -rf system
+  fi
+  if [ -d "vendor" ]; then
+    rm -rf vendor
+  fi
+  # Navigate to parent directory
+  cd ../
+}
+
+echo "### Git clone script for phone FP dumps ###"
+echo "Script by Juleast @ https://github.com/juleast"
+
+# main script
+read -p "Paste repo URL here: " url
 IFS='/' read -r -a url_arr <<< "$url"
 IFS='.' read -r -a git_dir_arr <<< "${url_arr[-1]}"
 git_dir=${git_dir_arr[0]}
@@ -14,37 +77,4 @@ branch_out=$(git branch)
 IFS=' ' read -r -a br_arr <<< "$branch_out"
 br=${br_arr[-1]}
 
-echo "### Try to get build.prop from system dirs ###"
-git checkout $br -- system/build.prop
-git checkout $br -- system/system/build.prop
-
-echo "### Try to get build.prop from system/product dirs ###"
-git checkout $br -- system/system/product/build.prop
-git checkout $br -- system/product/build.prop
-
-echo "### Try to get build.prop from vendor dirs ###"
-git checkout $br -- vendor/build.prop 
-
-
-echo Rename and move files...
-if [ -d "system/product" ]; then
-  mv system/product/build.prop ./product-build.prop
-elif [ -d "system/system/product" ]; then
-  mv system/system/product/build.prop ./product-build.prop
-fi
-
-if [ -d "system" ]; then
-  mv system/build.prop ./build.prop
-  if [ -d "system/system" ]; then
-    mv system/system/build.prop ./build.prop
-    rm -rf system
-  fi
-  rm -rf system
-fi
-
-if [ -d "vendor" ]; then
-  mv vendor/build.prop ./vendor-build.prop
-  rm -rf vendor
-fi
-
-cd ../
+checkout
--- gen_pif_custom.sh
+++ gen_pif_custom.sh
@@ -6,14 +6,7 @@
 
 echo "system build.prop to custom.pif.json/.prop creator \
     \n  by osm0sis @ xda-developers \
-    \n and modified by Juleast @ xda-developers";
-
-item() { echo "\n- $@"; }
-die() { 
-  echo "\n\n! $@"
-  return
-}
-file_getprop() { grep "^$2=" "$1" 2>/dev/null | tail -n1 | cut -d= -f2-; }
+    \n and modified by Juleast @ https://github.com/juleast";
 
 case $0 in
   *.sh) shdir="$0";;
@@ -34,105 +27,117 @@ else
 fi
 
 cd $shdir
+main()
 
-case $1 in
+item() { echo "\n- $@"; }
+die() { 
+  echo "\n\n! $@"
+  return
+}
+file_getprop() { grep "^$2=" "$1" 2>/dev/null | tail -n1 | cut -d= -f2-; }
+
+main() {
+  case $1 in
   json|prop) FORMAT=$1;;
   "") FORMAT=json;;
-esac;
-item "Using format: $FORMAT";
-
-[ ! -f build.prop ] && [ ! -f system-build.prop -o ! -f product-build.prop ] \
-   && die "No build.prop files found in script directory";
-
-item "Parsing build.prop(s) ...";
-
-PRODUCT=$(file_getprop build.prop ro.product.name);
-DEVICE=$(file_getprop build.prop ro.product.device);
-MANUFACTURER=$(file_getprop build.prop ro.product.manufacturer);
-BRAND=$(file_getprop build.prop ro.product.brand);
-MODEL=$(file_getprop build.prop ro.product.model);
-FINGERPRINT=$(file_getprop build.prop ro.build.fingerprint);
-
-[ -z "$PRODUCT" ] && PRODUCT=$(file_getprop build.prop ro.product.system.name);
-[ -z "$DEVICE" ] && DEVICE=$(file_getprop build.prop ro.product.system.device);
-[ -z "$MANUFACTURER" ] && MANUFACTURER=$(file_getprop build.prop ro.product.system.manufacturer);
-[ -z "$BRAND" ] && BRAND=$(file_getprop build.prop ro.product.system.brand);
-[ -z "$MODEL" ] && MODEL=$(file_getprop build.prop ro.product.system.model);
-[ -z "$FINGERPRINT" ] && FINGERPRINT=$(file_getprop build.prop ro.system.build.fingerprint);
-
-case $DEVICE in
-  generic) die "Generic /system/build.prop values found, rename to system-build.prop and add product-build.prop";;
-esac;
-
-[ -z "$PRODUCT" ] && PRODUCT=$(file_getprop product-build.prop ro.product.product.name);
-[ -z "$DEVICE" ] && DEVICE=$(file_getprop product-build.prop ro.product.product.device);
-[ -z "$MANUFACTURER" ] && MANUFACTURER=$(file_getprop product-build.prop ro.product.product.manufacturer);
-[ -z "$BRAND" ] && BRAND=$(file_getprop product-build.prop ro.product.product.brand);
-[ -z "$MODEL" ] && MODEL=$(file_getprop product-build.prop ro.product.product.model);
-[ -z "$FINGERPRINT" ] && FINGERPRINT=$(file_getprop product-build.prop ro.product.build.fingerprint);
-
-if [ -z "$FINGERPRINT" ]; then
-  if [ -f build.prop ]; then
-    die "No fingerprint found, use a /system/build.prop to start";
-  else
-    die "No fingerprint found, unable to continue";
-  fi;
-fi;
-echo "$FINGERPRINT";
-
-LIST="PRODUCT DEVICE MANUFACTURER BRAND MODEL FINGERPRINT";
-
-item "Parsing build UTC date ...";
-UTC=$(file_getprop build.prop ro.build.date.utc);
-[ -z "$UTC" ] && UTC=$(file_getprop system-build.prop ro.build.date.utc);
-date -u -d @$UTC;
-
-if [ "$UTC" -gt 1521158400 ]; then
-  item "Build date newer than March 2018, adding SECURITY_PATCH ...";
-  SECURITY_PATCH=$(file_getprop build.prop ro.build.version.security_patch);
-  [ -z "$SECURITY_PATCH" ] && SECURITY_PATCH=$(file_getprop system-build.prop ro.build.version.security_patch);
-  LIST="$LIST SECURITY_PATCH";
-  echo "$SECURITY_PATCH";
-fi;
-
-item "Parsing build first API level ...";
-FIRST_API_LEVEL=$(file_getprop vendor-build.prop ro.product.first_api_level);
-[ -z "$FIRST_API_LEVEL" ] && FIRST_API_LEVEL=$(file_getprop vendor-build.prop ro.board.first_api_level);
-[ -z "$FIRST_API_LEVEL" ] && FIRST_API_LEVEL=$(file_getprop vendor-build.prop ro.board.api_level);
-
-if [ -z "$FIRST_API_LEVEL" ]; then
-  [ ! -f vendor-build.prop ] && die "No first API level found, add vendor-build.prop";
-  item "No first API level found, falling back to build SDK version ...";
-  [ -z "$FIRST_API_LEVEL" ] && FIRST_API_LEVEL=$(file_getprop build.prop ro.build.version.sdk);
-  [ -z "$FIRST_API_LEVEL" ] && FIRST_API_LEVEL=$(file_getprop build.prop ro.system.build.version.sdk);
-  [ -z "$FIRST_API_LEVEL" ] && FIRST_API_LEVEL=$(file_getprop system-build.prop ro.build.version.sdk);
-  [ -z "$FIRST_API_LEVEL" ] && FIRST_API_LEVEL=$(file_getprop system-build.prop ro.system.build.version.sdk);
-  [ -z "$FIRST_API_LEVEL" ] && FIRST_API_LEVEL=$(file_getprop vendor-build.prop ro.vendor.build.version.sdk);
-  [ -z "$FIRST_API_LEVEL" ] && FIRST_API_LEVEL=$(file_getprop product-build.prop ro.product.build.version.sdk);
-fi;
-echo "$FIRST_API_LEVEL";
-
-if [ "$FIRST_API_LEVEL" -gt 32 ]; then
-  item "First API level 33 or higher, resetting to 32 ...";
-  FIRST_API_LEVEL=32;
-fi;
-LIST="$LIST FIRST_API_LEVEL";
-
-if [ -f custom.pif.$FORMAT ]; then
-  item "Removing existing custom.pif.$FORMAT ...";
-  rm -f custom.pif.$FORMAT;
-fi;
-
-item "Writing new custom.pif.$FORMAT ...";
-[ "$FORMAT" == "json" ] && echo '{' | tee -a custom.pif.json;
-for PROP in $LIST; do
-  case $FORMAT in
-    json) eval echo '\ \ \"$PROP\": \"'\$$PROP'\",';;
-    prop) eval echo $PROP=\$$PROP;;
   esac;
-done | sed '$s/,//' | tee -a custom.pif.$FORMAT;
-[ "$FORMAT" == "json" ] && echo '}' | tee -a custom.pif.json;
+  item "Using format: $FORMAT";
 
-echo
-echo "Done!"
+  [ ! -f build.prop ] && [ ! -f system-build.prop -o ! -f product-build.prop ] \
+    && die "No build.prop files found in script directory";
+
+  item "Parsing build.prop(s) ...";
+
+  PRODUCT=$(file_getprop build.prop ro.product.name);
+  DEVICE=$(file_getprop build.prop ro.product.device);
+  MANUFACTURER=$(file_getprop build.prop ro.product.manufacturer);
+  BRAND=$(file_getprop build.prop ro.product.brand);
+  MODEL=$(file_getprop build.prop ro.product.model);
+  FINGERPRINT=$(file_getprop build.prop ro.build.fingerprint);
+
+  [ -z "$PRODUCT" ] && PRODUCT=$(file_getprop build.prop ro.product.system.name);
+  [ -z "$DEVICE" ] && DEVICE=$(file_getprop build.prop ro.product.system.device);
+  [ -z "$MANUFACTURER" ] && MANUFACTURER=$(file_getprop build.prop ro.product.system.manufacturer);
+  [ -z "$BRAND" ] && BRAND=$(file_getprop build.prop ro.product.system.brand);
+  [ -z "$MODEL" ] && MODEL=$(file_getprop build.prop ro.product.system.model);
+  [ -z "$FINGERPRINT" ] && FINGERPRINT=$(file_getprop build.prop ro.system.build.fingerprint);
+
+  case $DEVICE in
+    generic) die "Generic /system/build.prop values found, rename to system-build.prop and add product-build.prop";;
+  esac;
+
+  [ -z "$PRODUCT" ] && PRODUCT=$(file_getprop product-build.prop ro.product.product.name);
+  [ -z "$DEVICE" ] && DEVICE=$(file_getprop product-build.prop ro.product.product.device);
+  [ -z "$MANUFACTURER" ] && MANUFACTURER=$(file_getprop product-build.prop ro.product.product.manufacturer);
+  [ -z "$BRAND" ] && BRAND=$(file_getprop product-build.prop ro.product.product.brand);
+  [ -z "$MODEL" ] && MODEL=$(file_getprop product-build.prop ro.product.product.model);
+  [ -z "$FINGERPRINT" ] && FINGERPRINT=$(file_getprop product-build.prop ro.product.build.fingerprint);
+
+  if [ -z "$FINGERPRINT" ]; then
+    if [ -f build.prop ]; then
+      die "No fingerprint found, use a /system/build.prop to start";
+    else
+      die "No fingerprint found, unable to continue";
+    fi;
+  fi;
+  echo "$FINGERPRINT";
+
+  LIST="PRODUCT DEVICE MANUFACTURER BRAND MODEL FINGERPRINT";
+
+  item "Parsing build UTC date ...";
+  UTC=$(file_getprop build.prop ro.build.date.utc);
+  [ -z "$UTC" ] && UTC=$(file_getprop system-build.prop ro.build.date.utc);
+  date -u -d @$UTC;
+
+  if [ "$UTC" -gt 1521158400 ]; then
+    item "Build date newer than March 2018, adding SECURITY_PATCH ...";
+    SECURITY_PATCH=$(file_getprop build.prop ro.build.version.security_patch);
+    [ -z "$SECURITY_PATCH" ] && SECURITY_PATCH=$(file_getprop system-build.prop ro.build.version.security_patch);
+    LIST="$LIST SECURITY_PATCH";
+    echo "$SECURITY_PATCH";
+  fi;
+
+  item "Parsing build first API level ...";
+  FIRST_API_LEVEL=$(file_getprop vendor-build.prop ro.product.first_api_level);
+  [ -z "$FIRST_API_LEVEL" ] && FIRST_API_LEVEL=$(file_getprop vendor-build.prop ro.board.first_api_level);
+  [ -z "$FIRST_API_LEVEL" ] && FIRST_API_LEVEL=$(file_getprop vendor-build.prop ro.board.api_level);
+
+  if [ -z "$FIRST_API_LEVEL" ]; then
+    [ ! -f vendor-build.prop ] && die "No first API level found, add vendor-build.prop";
+    item "No first API level found, falling back to build SDK version ...";
+    [ -z "$FIRST_API_LEVEL" ] && FIRST_API_LEVEL=$(file_getprop build.prop ro.build.version.sdk);
+    [ -z "$FIRST_API_LEVEL" ] && FIRST_API_LEVEL=$(file_getprop build.prop ro.system.build.version.sdk);
+    [ -z "$FIRST_API_LEVEL" ] && FIRST_API_LEVEL=$(file_getprop system-build.prop ro.build.version.sdk);
+    [ -z "$FIRST_API_LEVEL" ] && FIRST_API_LEVEL=$(file_getprop system-build.prop ro.system.build.version.sdk);
+    [ -z "$FIRST_API_LEVEL" ] && FIRST_API_LEVEL=$(file_getprop vendor-build.prop ro.vendor.build.version.sdk);
+    [ -z "$FIRST_API_LEVEL" ] && FIRST_API_LEVEL=$(file_getprop product-build.prop ro.product.build.version.sdk);
+  fi;
+  echo "$FIRST_API_LEVEL";
+
+  if [ "$FIRST_API_LEVEL" -gt 32 ]; then
+    item "First API level 33 or higher, resetting to 32 ...";
+    FIRST_API_LEVEL=32;
+  fi;
+  LIST="$LIST FIRST_API_LEVEL";
+
+  if [ -f custom.pif.$FORMAT ]; then
+    item "Removing existing custom.pif.$FORMAT ...";
+    rm -f custom.pif.$FORMAT;
+  fi;
+
+  item "Writing new custom.pif.$FORMAT ...";
+  [ "$FORMAT" == "json" ] && echo '{' | tee -a custom.pif.json;
+  for PROP in $LIST; do
+    case $FORMAT in
+      json) eval echo '\ \ \"$PROP\": \"'\$$PROP'\",';;
+      prop) eval echo $PROP=\$$PROP;;
+    esac;
+  done | sed '$s/,//' | tee -a custom.pif.$FORMAT;
+  [ "$FORMAT" == "json" ] && echo '}' | tee -a custom.pif.json;
+
+  echo
+  echo "Done!"
+}
+
+
 cd ../
